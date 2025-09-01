891cae65f59c3a58f7b9153418eeb1ae859a7f9f
Lehlohonolo Motshoane <lehlohonolomotshoane@Lehlohonolos-MacBook-Air.local>
2025-09-01 09:22:24 +0200

    chore: initial Next.js project setup with TypeScript and Tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1 className="text-3xl font-bold underline">Hello, Advanced intelligence system!</h1>
+    </div>
+  );
+}
